243823c66299ad063ec7e25f077fa0ee795448ae
Shahar Shechter <shahars3@localhost.localdomain>
2020-12-08 18:39:42 +0200

    Removed unwanted code and template

--- SRC/APPLICATION-SOURCE-CODE/server.py
+++ SRC/APPLICATION-SOURCE-CODE/server.py
@@ -6,13 +6,6 @@ import mysql.connector
 db = mysql.connector.connect(host=config.DB_SERVER, user=config.DB_USERNAME, password=config.DB_PASSWORD, database=config.DB_SCHEMA)
 app = Flask(__name__)
 
-@app.route('/search')
-def search_return_html():
-    query = request.args.get('query')
-    # with connector get to your mysql server and query the DB
-    # return the answer to number_of_songs var.
-    number_of_songs = 5 #should be retrieved from the DB
-    return render_template('searchResults.html', count=5, query=query)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():

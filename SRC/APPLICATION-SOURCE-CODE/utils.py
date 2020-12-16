e8105da542fd44e2d874ac18d46868a179549c63
Yuval Dvir <dvir.yuval@gmail.com>
2020-12-16 16:54:10 +0200

    Yuval reupload after local checking

--- SRC/APPLICATION-SOURCE-CODE/utils.py
+++ SRC/APPLICATION-SOURCE-CODE/utils.py
@@ -2,8 +2,7 @@ import random
 
 NUMBER_OF_ACTORS_IN_DB = 131788
 NUMBER_OF_MOVIES_IN_DB = 10000
-NUMBER_OF_GENRES_IN_DB = 10770
-GENRES = [12,14,16,18,27,28,35,36,37,53,80,99,878,9648,10402,10749,10751,10752,10770]
+NUMBER_OF_GENRES_IN_DB = 18
 
 def run_sql_file(db, sql_path, **fmt):
     cur = db.cursor()
@@ -20,4 +19,4 @@ def get_random_movie_token():
     return random.randint(0, NUMBER_OF_MOVIES_IN_DB)
 
 def get_random_genre_token():
-    return random.choice(GENRES)
+    return random.randint(0, NUMBER_OF_GENRES_IN_DB)

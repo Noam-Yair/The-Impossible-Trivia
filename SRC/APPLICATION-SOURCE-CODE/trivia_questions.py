487feb25671d8af1db27d9724e810005dd70ae2b
Noam-Yair <64407908+Noam-Yair@users.noreply.github.com>
2020-12-25 13:21:03 +0200

    Noam added two functions according to the queries

--- SRC/APPLICATION-SOURCE-CODE/trivia_questions.py
+++ SRC/APPLICATION-SOURCE-CODE/trivia_questions.py
@@ -32,3 +32,40 @@ def genre_which_actor_x_is_most_identified_with(db):
         "option2": d[4],
         "option3": d[5],
     }
+
+def avg_movie_runtime(db):
+    d = utils.run_sql_file(db,
+                           "sqls/avg_movie_runtime.sql",
+                           actor_token1=utils.get_random_actor_token(),
+                           actor_token2=utils.get_random_actor_token(),
+                           actor_token3=utils.get_random_actor_token(),)
+
+    return {
+        "question": d[0],
+        "answer": d[1],
+        "img": None,
+        "option1":  d[1]-1,
+        "option2": d[1]+2,
+        "option3": d[1]+1,
+    }
+
+def num_of_actors_from_gender_x_who_played_in_lead_roles_in_movie_y(db):
+    options = [0, 1, 2, 3]
+    movies_token = utils.get_random_movie_token()
+    gender_token = utils.get_random_gender_token()
+    d = utils.run_sql_file(db,
+                           "sqls/num_of_actors_from_gender_x_who_played_in_lead_roles_in_movie_y.sql",
+                           movies_token=movies_token,
+                           gender_token=gender_token)
+    if d[0]:
+        return {
+            "question": d[0],
+            "answer": options.pop(d[1]),
+            "img": d[2],
+            "option1": options.pop(),
+            "option2": options.pop(),
+            "option3": options.pop(),
+        }
+    else:
+        return num_of_actors_from_gender_x_who_played_in_lead_roles_in_movie_y(db)
+

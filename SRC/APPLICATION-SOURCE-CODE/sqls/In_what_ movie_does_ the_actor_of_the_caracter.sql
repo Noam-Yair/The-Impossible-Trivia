66041e75775b76f2727f842058ada2059fc2266a
asheri1 <65289136+asheri1@users.noreply.github.com>
2021-01-03 14:11:07 +0200

    or's query

---
+++ SRC/APPLICATION-SOURCE-CODE/sqls/In_what_ movie_does_ the_actor_of_the_caracter.sql
@@ -0,0 +1,31 @@
+SELECT
+    CONCAT("In what movie does the actor of the caracter '", ANY_VALUE(movie_actors.character_name),
+    "' in the movie '", ANY_VALUE(movies.title), "' has made his first appeariance?") as question,
+    movies.title AS question_movie,
+    actors.profile_path AS img,
+    actor_id AS our_ACTOR_id,
+    
+	(SELECT movies.title
+	FROM
+		movies, movie_actors
+	WHERE
+		movies.id = movie_actors.movie_id AND
+        movie_actors.actor_id = our_ACTOR_id
+	ORDER BY release_date
+	LIMIT 1 ) as answer,
+   
+    (SELECT movies.title FROM movies WHERE movies.rnd_token = {movie_token1}) as option1,
+    (SELECT movies.title FROM movies WHERE movies.rnd_token = {movie_token2}) as option2,
+    (SELECT movies.title FROM movies WHERE movies.rnd_token = {movie_token3}) as option3
+    
+FROM
+	actors
+        LEFT JOIN
+    movie_actors ON actors.id = movie_actors.actor_id
+        LEFT JOIN
+    movies ON movies.id = movie_actors.movie_id
+WHERE
+movies.rnd_token = {movie_token}
+ORDER BY release_date
+LIMIT 1
+

359cba8c2770dc5d1119e56930f855ac50640dea
Shahar Shechter <shahars3@localhost.localdomain>
2021-01-08 12:59:31 +0200

    Removed bad line

--- SRC/APPLICATION-SOURCE-CODE/trivia_questions.py
+++ SRC/APPLICATION-SOURCE-CODE/trivia_questions.py
@@ -18,7 +18,6 @@ def movie_from_genre_x_where_y_played_for_the_first_time(db):
 
 
 def genre_which_actor_x_is_most_identified_with(db):
-    genres_tokens = get_random_genre_tokens_list
     d = utils.run_sql_file(db,
                            "sqls/genre_which_actor_x_is_most_identified_with.sql",
                            actor_token=utils.get_random_actor_token())
